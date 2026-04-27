7cd01f770f6db57bddb67b0f4bbea8597298d9ca
Bryan Z. <bryan.bigjazzdaddy@gmail.com>
2026-04-27 13:36:21 -0700

    innitial commet, and build structure

---
+++ src/main/java/com/pluralsight/Front.java
@@ -0,0 +1,68 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Front
+{
+    Scanner userInput = new Scanner();
+    static void main()
+    {
+        System.out.println("~~~~~~~~");
+        System.out.println("Welcome");
+        System.out.println("~~~~~~~~");
+        System.out.println("what would you like to do?");
+        System.out.println();
+        System.out.println("a");
+
+
+        switch()
+        {
+            case "1";
+            System.out.println("1) Make a Deposit");
+            break;
+
+
+        }
+        //display home screen
+
+    }
+
+//FileReader fileReader = new FileReader();
+//BuffReader buffReader = new BufferedReader(fileReader);
+
+    //home screen
+        //display welcome
+        //prompt user selection
+            //select 1) 2) 3) menu options
+            //switch loop
+            //if not then print wrong options.
+            //prompt exit
+        //go to options
+            // deposite option
+            //make payment
+        //go to ledger
+
+
+    //ledger
+        //ledger display
+        //entries, newist first
+        //deposites
+        //payments
+
+        //reports
+            //reports display
+            //month to date
+            //previous month
+            //year to date
+            //previous year
+            //search by vendor
+            //return to ledger
+
+
+
+    //exit
+        //prompt the user if they want to exit the program
+
+}
