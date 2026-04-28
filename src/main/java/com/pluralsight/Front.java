3cf7e632bae2ddf1e6822daa6026a940fcc99a2e
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-04-28 10:32:19 -0800

    fixed errors, added csv file

--- src/main/java/com/pluralsight/Front.java
+++ src/main/java/com/pluralsight/Front.java
@@ -6,55 +6,10 @@ import java.util.Scanner;
 
 public class Front {
 
-    static void main() {
-        //call for csv file to be read
-        try {
-            FileReader fileReader = new FileReader("transactions.csv");
-            Scanner userInput = new Scanner(System.in);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+    static void main()
+    {
 
-            while (true) {
-                System.out.println("\n===== Home Screen =====");
-                System.out.println("D) Add Deposit");
-                System.out.println("P) Make Payment");
-                System.out.println("L) Ledger");
-                System.out.println("X) Exit");
-                System.out.print("Choose: ");
-                String choice = userInput.nextLine().trim().toUpperCase();
-
-                switch (choice) {
-                    case "D":
-                        System.out.println("Add a Deposit");
-                        //      addDeposit();
-                        //array
-                    case "P":
-                        System.out.println("Make a payment selected");
-                        //      makePayment();
-                        //array
-                    case "L":
-                        System.out.println("Display My ledger");
-                        //array
-                        //      ledgerScreen();
-                    case "X":
-
-                        System.out.println("Goodbye!");
-                        return;
-                    // exits homeScreen → ends program
-
-                    default:
-                        System.out.println("Invalid option.");
-                }
-
-
-            }
-            //display home screen
-
-        } catch (Exception ex) {
-
-        }
-
-//FileReader fileReader = new FileReader();
-//BuffReader buffReader = new BufferedReader(fileReader);
+    }
 
         //home screen
         //display welcome
