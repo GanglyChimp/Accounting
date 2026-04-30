b2d0e444e6ea07749b7e52c1ecc1ab154b4a3213
Bryan Z. <bryan.bigjazzdaddy@gmail.com>
2026-04-30 14:00:53 -0700

    adding statics formating and scanner for deposit

--- src/main/java/com/pluralsight/addDeposite.java
+++ src/main/java/com/pluralsight/addDeposite.java
@@ -1,5 +1,22 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
 public class addDeposite
 {
+    static final String fileName = "transactions.csv";
+    //defining the date and time/  hours:min:seconds
+    //formating the date as yyyy-mm--dd
+    static final DateTimeFormatter dateClockTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+    static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    //scanner for userInput
+    static Scanner scanner = new Scanner(System.in);
+
 }
